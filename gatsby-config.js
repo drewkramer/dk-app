443913712f50655f3181d090c2e27488dd2ee661
Drew Kramer <drewkramer@Tylers-MacBook-Pro.local>
2018-09-01 20:55:39 -0400

    first file push

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'pobaqw2pmlpb',
+        accessToken: '074b98830a320f539708b45fd9ff2fb1d577783e9bf8def73965bcd0a94a146b'
+      }
+    }
+  ],
+}
